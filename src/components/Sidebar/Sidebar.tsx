56af84f2b8db18e0ec70118a69ba04fb394d2eb4
Leoodaviid <david_ax7@hotmail.com>
2023-02-09 00:30:47 -0300

    Incluindo restante do menu/icones

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,19 +1,47 @@
+import { useState } from 'react'
 import { Container } from './styles'
-import menuImg from '../../assets/menu.svg'
 import { ReactComponent as BurguerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
+import menuImg from '../../assets/menu.svg'
 
 const Sidebar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
-    <Container>
-      <button type='button'>
+    <Container isMenuOpen={menuOpen}>
+      <button type='button' onClick={handleToggleMenu}>
         <img src={menuImg} alt='Abrir e fechar o menu' />
       </button>
       <nav>
         <ul>
           <li>
-            <a href='#'>
+            <a href='#' className='active'>
               <BurguerIcon />
-              <span>Hambúrguer</span>
+              <span>Hambúrgueres</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <PizzaIcon />
+              <span>Pizzas</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <SodaIcon />
+              <span>Bebidas</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <IceCreamIcon />
+              <span>Sorvetes</span>
             </a>
           </li>
         </ul>

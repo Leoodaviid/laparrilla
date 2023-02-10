5a5d9299b98cbe3db962f1a41f6f3c1345bb6306
Leoodaviid <david_ax7@hotmail.com>
2023-02-10 05:38:22 -0300

    Estruturando componente de loading skeleton

---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,9 @@
+import { SkeletonElement } from './styles'
+
+export interface SkeletonProps {
+  type: 'title' | 'text' | 'image' | 'thumbnail'
+}
+
+export const Skeleton = ({ type }: SkeletonProps) => {
+  return <SkeletonElement type={type} />
+}

04c1b52595dab120d87ff0161d47d7d4cb8e1e91
Leoodaviid <david_ax7@hotmail.com>
2023-02-10 05:38:48 -0300

    Criando elemento de wrapper do loading

---
+++ src/components/Snacks/SkeletonSnacks/SkeletonSnack.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from '../../Skeleton/Skeleton'
+import { Container } from '../styles'
+
+export const SkeletonSnack = () => {
+  return (
+    <div className='skeletonWrapper'>
+      <Container>
+        <Skeleton type='title' />
+        <Skeleton type='thumbnail' />
+        <Skeleton type='text' />
+        <Skeleton type='text' />
+        <div>
+          <Skeleton type='title' />
+          <Skeleton type='image' />
+        </div>
+      </Container>
+    </div>
+  )
+}
